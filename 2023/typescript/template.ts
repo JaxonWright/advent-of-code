1a63010a32ea2ffde2378ea810489c7184749349
Jaxon Wright <jaxonhwright@gmail.com>
2023-12-05 23:05:58 -0500

    2023: add TS template file

---
+++ 2023/typescript/template.ts
@@ -0,0 +1,25 @@
+import * as fs from "fs";
+import * as readline from 'readline';
+
+/**
+ * Advent of Code 2023 - Day #, Puzzle #
+ * 
+ * Short description of problem
+ * 
+ * https://adventofcode.com/2023/day/#
+ */
+
+const PUZZLE_INPUT_PATH = '../inputs/#-#.txt';
+
+const lineReader = readline.createInterface({
+    input: fs.createReadStream(PUZZLE_INPUT_PATH),
+    terminal: false,
+});
+
+lineReader.on('line', (line) => {
+   // read lines
+});
+
+lineReader.on('close',()=> {
+    // print answer
+})
